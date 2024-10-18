d7474307795f459a256a7b65494168b590a0619e
Abraham Lara Rodriguez <Abrahamopelara@hotmail.com>
2024-10-18 02:59:34 -0500

    Creating REST API for book management and file renaming.

---
+++ msvc-books/src/main/java/org/dev/library/msvc/books/dev/MsvcBooksApplication.java
@@ -0,0 +1,13 @@
+package org.dev.library.msvc.books.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcBooksApplication.class, args);
+	}
+
+}
